89fb19e02121223100e5272f9a96443417e018e9
Shrikant Humbe <162597160+LUCARIO-7@users.noreply.github.com>
2026-07-06 20:02:48 +0530

    Revert "first commit to vibe coded branch"

--- src/components/Founditems.vue
+++ src/components/Founditems.vue
@@ -1,7 +1,7 @@
 <script setup>
 import axios from 'axios';
 import { ref } from 'vue';
-
+import searchTab from './Navigation/searchTab.vue';
 const items=ref();
 const baseimage=ref();
 async function fetchItems(){
@@ -15,57 +15,28 @@ async function claimItem(itemid){
     console.log(error);
   }
 }
-async function handleFilter(num) {
-  var location;
-  if(num=='1'){
-  location="monaco";
-}
-else{
-  location="";
-}
-try {
-    const response=await axios.get(`http://localhost:8081/founditems/filter`,{
-      params:{
-        "location":location,
-        "time":""
-      }
-    });
-    items.value=response.data;
-  } catch (error) {
-    console.log(error);
-  }
-}
 fetchItems();
 </script>
 <template>
-   <div class="dropdown">
-  <div tabindex="0" role="button" class="btn m-1">Location</div>
-  <ul tabindex="-1" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-    <li @click="handleFilter('0')"><a>all Locations</a></li>
-    <li @click="handleFilter('1')"><a>monaco</a></li>
-  </ul>
-</div>
-   <div class="grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-   <div v-for="item in items" :key="item.id" >
-     <div class="card card-side bg-base-100 w-full shadow-md">
+  <searchTab/>
+  <div class="flex justify-center items-center flex-col">
+   <ul v-for="item in items" :key="item.id">
+     <div class="card bg-base-100 w-150 shadow-sm  mb-5">
   <figure>
-    <img :src="`data:image/png;base64,${item.image}`" class=" object-cover h-60 w-60" />
+    <img :src="`data:image/png;base64,${item.image}`" class="object-cover h-120 w-240" />
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">{{ item.name }}</h2>
+  <div class="card-body bg-white rounded-b-sm text-black">
+    <h2 class="card-title">{{item.name }}</h2>
+    <li class="list-row">
+      <div class=" badge badge-neutral">posted by:{{item.user.username}}</div>
+      <div class=" badge badge-info">time found:{{item.time}}</div>
+      </li>
     <p>{{ item.description }}</p>
-    <div class="flex items-center gap-1.5   text-sm">
-  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-  </svg>
-  <p>{{ item.time }}</p>
-</div>
-    <div class="card-actions justify-end">
-      <button v-if="item.isClaimed" class="btn btn-disabled" @click="claimItem">claimed</button>
-      <button v-else class="btn btn-primary" @click="claimItem(item.id)">claim</button>
-    </div>
-    </div>
+    <div class="badge badge-neutral badge-outline">{{ item.contact }}</div>
+   <button v-if="item.isClaimed" class="btn btn-disabled bg-gray-600">claimed</button>
+   <button v-else class="btn btn-primary bg-gray-800" @click="claimItem(item.id)">claim</button>
   </div>
-   </div>
+</div>
+   </ul>
    </div>
 </template>
--- src/components/Navigation/navBar.vue
+++ src/components/Navigation/navBar.vue
@@ -1,14 +1,7 @@
-<script setup lang="ts">
-import SearchTab from './searchTab.vue';
-
-</script>
-
 <template>
-  <div class="navbar bg-base-100 shadow-sm mb-5 text-black ">
+  <div class="navbar bg-base-100 shadow-sm bg-gray-900 mb-5">
     <div class="navbar-start">
-      <div class="bg-blue-600 text-white font-bold px-5 py-2 rounded-lg text-lg uppercase tracking-wide shadow-sm ">
-          <a href="/">DIBS</a>
-        </div>
+      <a class="text-xl text-white" href="/">Dibs</a>
     </div>
     
     <div class="navbar-center">
@@ -16,12 +9,10 @@ import SearchTab from './searchTab.vue';
         <li><a href="/">home</a></li>
         <li><a href="/founditems">browse</a></li>
         <li><a href="/additem">post</a></li>
-        
       </ul>
     </div>
     <div class="navbar-end" >
-  <SearchTab/>
-<a href="/user" class="block hover:opacity-80 transition duration-200 ml-4">
+<a href="/user" class="block hover:opacity-80 transition duration-200">
   <div class="avatar">
     <div class="w-16 rounded-full">
       <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" alt="Profile" />
